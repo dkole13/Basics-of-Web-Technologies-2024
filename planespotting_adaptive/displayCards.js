3799f0ba2923745ca0f848a7652d17ce21a75fab
dkole13 <dasha10206@gmail.com>
2024-12-03 21:40:55 +0300

    6 cards from json

---
+++ planespotting_adaptive/displayCards.js
@@ -0,0 +1,29 @@
+async function displayCards() {
+    let data = await fetch("data.json", {
+        method: 'GET',
+        mode: 'cors',
+        // body: JSON.stringify(data)
+    }
+    );
+    data = JSON.stringify(data);
+    console.log(data);
+
+    const cardGroup = document.getElementById('card-group');
+    cardGroup.innerHTML = ``
+
+    data.forEach(element => {
+        const card = document.createElement('div');
+
+        card.innerHTML = `
+        <div class="card">
+            <img class="card-img-top" src="${element.imageSrc}" alt="${element.imageAlt}">
+            <div class="card-body">
+                <a class="card-title" href="${element.link}" target="_blank">${element.name}</a>
+                <p class="card-text">${element.description}</p>
+            </div>
+        </div>
+        `;
+    });
+}
+
+displayCards()
